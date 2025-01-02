957495b7ffc856f4e42a65b815096c89611299f5
mattgithub2 <mattwarner744@gmail.com>
2025-01-02 14:36:09 -0600

    ...

--- lua/cs/theme.lua
+++ lua/cs/theme.lua
@@ -8,27 +8,40 @@ function theme.setup()
 
     theme.highlights = {
         -- Base highlights
-        Boolean = { fg = color.color5 },
-        Character = { fg = color.color2 },
-        Comment = { fg = color.comment},
+        Normal = { fg = color.foreground, bg = color.background },
+        NormalFloat = { link = "Normal" },
+
+        Boolean = { link = "String" },
+        Character = { link = "String" },
+        Comment = { fg = color.comment },
         Conditional = { fg = color.color12, bold = true },
         Constant = { fg = color.color3 },
-        CursorLine = { bg = color.cursorline },
-        CursorLineNr = { fg = color.foreground, bg = color.cursorline },
-        Directory = { fg = color.color4 },
-        Error = { fg = color.color1 },
-        Function = { fg = color.color16,},
-        Identifier = { fg = color.color7 },
-        Keyword = { fg = color.color12},
-        LineNr = { fg = color.color8 },
-        Normal = { fg = color.foreground, bg = color.background },
-        Pmenu = { fg = color.color6, bg = color.cursorline },
-        PmenuSel = { fg = color.color1, bg = color.color6 },
-        Search = { fg = color.color1, bg = color.color4 },
         String = { fg = color.color2 },
-        Title = { fg = color.color4, bold = true },
+        Function = { fg = color.color16,},
+        Error = { fg = color.color8 },
+        Keyword = { fg = color.color12 },
+        Delimiter = { fg = color.color16 },
+        Operator = { fg = color.color16 },
         Type = { fg = color.color3, bold = true},
-        Visual = { bg = color.color0 },
+        Identifier = { fg = color.color16 },
+        Todo = { fg = color.color11 },
+        PreProc = { fg = color.color4 },
+
+        MatchParen = { fg = color.color3, bold = true },
+
+
+        LineNr = { fg = color.color8 },
+
+        CursorLine = { bg = color.cursorline },
+        CursorLineNr = { fg = color.black, bg = color.cursorline },
+        Directory = { fg = color.color4 },
+
+        Pmenu = { fg = color.black, bg = color.background },
+        PmenuSel = { fg = color.color1, bg = color.color6 },
+
+        Search = { fg = color.color1, bg = color.color4 },
+        Title = { fg = color.color4, bold = true },
+        Visual = { bg = color.color6 },
         WarningMsg = { fg = color.color3 },
 
         -- LSP
@@ -37,9 +50,13 @@ function theme.setup()
         DiagnosticInfo = { fg = color.color4 },
         DiagnosticHint = { fg = color.color2 },
 
-        -- Plugins: Telescope
-        TelescopeBorder = { fg = color.color7 },
-        TelescopeSelection = { fg = color.color6, bg = color.cursorline },
+        -- Treesitter
+        ["@variable"] = { link = "Identifier"},
+        ["@variable.builtin"] = { link = "Type" },
+        ["@variable.parameter"] = { link = "Identifier"},
+        ["@variable.member"] = { link = "Identifier"}
+
+
     }
 
     for group, hl in pairs(theme.highlights) do
